e0bf815aad1b517646a90ba2bc8e96bdbc25e0d2
rvansyah <irvvnsyh@gmail.com>
2025-07-15 10:40:44 +0700

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -2,10 +2,8 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import pickle
 
-# Load dataset
-df = pd.read_csv('house_prices.csv')   # Pastikan file house_prices.csv sudah ada di folder
-
-# Pilih minimal 2 fitur dan 1 target
+# Load data
+df = pd.read_csv('house_prices.csv')
 X = df[['LotArea', 'BedroomAbvGr', 'YearBuilt']]
 y = df['SalePrice']
 
@@ -13,6 +11,6 @@ y = df['SalePrice']
 model = LinearRegression()
 model.fit(X, y)
 
-# Simpan model ke file
+# Simpan model ke file model.pkl
 with open('model.pkl', 'wb') as f:
     pickle.dump(model, f)
